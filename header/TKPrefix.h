c05456939207e9c2e9e1744ab94b30b113ad9418
rocket99 <rocket99ios@126.com>
2021-02-18 16:39:06 +0800

    add code

---
+++ header/TKPrefix.h
@@ -0,0 +1,37 @@
+//
+//TKPrefix.h
+//
+
+#ifndef __TKPrefix_h__
+#define __TKPrefix_h__
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <list>
+#include <functional>
+#include <chrono>
+
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <glm/common.hpp>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_keyboard.h>
+#include <SDL2/SDL_video.h>
+#include <SDL2/SDL_vulkan.h>
+
+#include <vulkan/vulkan.h>
+#include <vulkan/vk_layer.h>
+#include <vulkan/vk_platform.h>
+
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "TKPrefix.h"
+
+int main(int argc, char *argv[])
+{
+	printf("Real Project Start!\n");
+	return 0;
+}
